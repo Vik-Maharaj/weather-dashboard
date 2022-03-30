c9d65be7c6f2aacba8cac9b5bea0f52515af464b
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-03-30 15:37:18 -0400

    Psuedo-code js logic

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// js for city search user text input
+
+
+// js for city search button
+
+
+// js for displaying the eight most recently viewed cities
+
+
+// js for setting the currently displayed city as the user inputted/searched city
+
+
+// js for fetching and displaying the current weather information for that displayed city
+
+
+// js for the current weather icon for the currently displayed city
+
+
+// js for the color coded uv indicator
+
+
+// js for displaying the five day forecasts for the currently displayed city
+
+
+// js for setting the most recently displayed cities into local storage, and then to retrieve the local storage data 
+
+
+// js for clicking on the eight most recently viewed cities to then be selected as the currently displayed city
+
