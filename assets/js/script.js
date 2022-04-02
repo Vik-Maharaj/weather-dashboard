f28991cc35a0b85245a91b956a56a73cdb44b218
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-04-02 16:16:56 -0400

    Declare js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,13 @@
-// js for city search user text input
+//declare and initialize variables
 
-
-// js for city search button
-
-
-// js for displaying the eight most recently viewed cities
-
-
-// js for setting the currently displayed city as the user inputted/searched city
-
-
-// js for fetching and displaying the current weather information for that displayed city
-
-
-// js for the current weather icon for the currently displayed city
-
-
-// js for the color coded uv indicator
-
-
-// js for displaying the five day forecasts for the currently displayed city
-
-
-// js for setting the most recently displayed cities into local storage, and then to retrieve the local storage data 
-
-
-// js for clicking on the eight most recently viewed cities to then be selected as the currently displayed city
+var search = document.getElementById("search");
+var city = document.getElementById("city");
+var citySearch = document.getElementById("city-search")
+var currentCityContainer = document.getElementById("current-city-container");
+var displayedCity = document.getElementById("displayed-city");
+var displayWeather = document.getElementById("displayWeather");
+var myCities = document.getElementById("myCities");
+var deleteButton = document.getElementById("deleteButton");
+var day = document.getElementById("day1");
+var date = document.getElementById("date1");
 
