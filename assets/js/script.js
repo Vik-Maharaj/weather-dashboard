074686d79712763b079ec2c2e4c4a7bc524d9f89
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-04-03 13:04:23 -0400

    Add additional api fetch js

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,56 @@ var findCityName = function(searchedCity) {
         }
     })
 };
+
+
+
+// using the fetched latitude and longitude info, fetches current weather and five day forecast for input city and displays current weather info
+
+var findCityData = function (lat, lon) {
+    var apiCityData = "https://api.openweathermap.org/data/2.5/onecall?&lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=07bdc8ba3d40336f00288a88b3d7d15f&units=imperial"
+
+    fetch(apiCityData).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayWeather.innerHTML = "";
+
+                var currentWeatherIcon = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
+                var weatherIcon = document.createElement("img")
+                weatherIcon.setAttribute("src", currentWeatherIcon);
+                weatherIcon.classList.add("icon");
+                displayWeather.appendChild(weatherIcon);
+
+                var currentTemp = data.current.temp;
+                var temp = document.createElement("p")
+                temp.textContent = "Temp:" + " " + currentTemp + " " + "°F";
+                temp.classList.add("infoContent");
+                displayWeather.appendChild(temp);
+
+                var currentWind = data.current.wind_speed;
+                var wind = document.createElement("p")
+                wind.textContent = "Wind:" + " " + currentWind + " " + "MPH";
+                wind.classList.add("infoContent");
+                displayWeather.appendChild(wind);
+
+                var currentHumidity = data.current.humidity;
+                var humidity = document.createElement("p")
+                humidity.textContent = "Humidity:" + " " + currentHumidity + " " + "%";
+                humidity.classList.add("infoContent");
+                displayWeather.appendChild(humidity);
+
+                var currentUvi = data.daily[0].uvi;
+                var uvi = document.createElement("p");
+                uvi.classList.add("infoContent");
+                uvi.textContent = "UV Index:" + " " + currentUvi;
+                displayWeather.appendChild(uvi);
+
+                fiveDayForecast(data.daily);
+            });
+        } else {
+            alert("Error: City Not Found");
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to Connect");
+        })
+};
